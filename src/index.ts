46769676f5065e1587e36d42ef74319126d5010c
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-12-03 11:35:59 -0400

    Improve the way the module is exported
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
 import { extractMessage } from 'error-message-utils';
-
-/* ************************************************************************************************
- *                                             TYPES                                              *
- ************************************************************************************************ */
-
-/**
- * API Response
- * The response object that is sent to the client via the HTTP body.
- */
-interface IAPIResponse {
-  // a response is considered to be successful if error === undefined
-  success: boolean,
-
-  // the data that will be sent to the client (regardless of the request's outcome)
-  data: any,
-
-  // the error thrown during the handling of the request (if any)
-  error: string | undefined
-}
-
-
-
+import { IAPIResponse } from './types.js';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
@@ -62,7 +41,7 @@ const isResponse = (response: any): response is IAPIResponse => (
  ************************************************************************************************ */
 export {
   // types
-  IAPIResponse,
+  type IAPIResponse,
 
   // implementation
   buildResponse,
---
+++ src/types.ts
@@ -0,0 +1,31 @@
+
+
+/* ************************************************************************************************
+ *                                             TYPES                                              *
+ ************************************************************************************************ */
+
+/**
+ * API Response
+ * The response object that is sent to the client via the HTTP body.
+ */
+interface IAPIResponse {
+  // a response is considered to be successful if error === undefined
+  success: boolean,
+
+  // the data that will be sent to the client (regardless of the request's outcome)
+  data: any,
+
+  // the error thrown during the handling of the request (if any)
+  error: string | undefined
+}
+
+
+
+
+
+/* ************************************************************************************************
+ *                                         MODULE EXPORTS                                         *
+ ************************************************************************************************ */
+export type {
+  IAPIResponse,
+};
